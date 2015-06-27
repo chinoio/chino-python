6fd6f7672dfb24ccd460353863737abdc897972c
Stefano <stefano.tranquillini@gmail.com>
2015-06-27 17:55:18 +0200

    removing old stuff

--- chino/objects.py
+++
@@ -1,9 +0,0 @@
-from collections import namedtuple
-
-__author__ = 'Stefano Tranquillini <stefano.tranquillini@gmail.com>'
-
-# NOTE: namedtuple in case we want classes
-# ListResult = namedtuple("ListResult",['results','count','total_count','size','offset'])
-# Repository = namedtuple('Repository', ['insert_date', 'repository_id', 'is_active', 'description', 'last_update'])
-# Document = namedtuple('Document', ["repository_id", "insert_date", "is_active", "last_update", "content", "schema_id",
-#                                    "document_id"])
