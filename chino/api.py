cc404e82de1a7f2be7acd45072ce71efba3d8b87
Stefano <stefano.tranquillini@gmail.com>
2016-05-24 09:48:14 +0200

    Update api.py
    config

--- chino/api.py
+++ chino/api.py
@@ -23,7 +23,7 @@ import logging.config
 __author__ = 'Stefano Tranquillini <stefano@chino.io>'
 
 logger = logging.getLogger(__name__)
-# logging.config.fileConfig('../logging.conf')
+logging.config.fileConfig('../logging.conf')
 logger.setLevel(logging.DEBUG)
 
 class ChinoAPIBase(object):  # PRAGMA: NO COVER
