386932af8fcc37bbc5441aaeaafb8ae0b779f3b9
Stefano Tranquillini <stefano.tranquillini@gmail.com>
2016-05-30 15:47:44 +0200

    fix?

--- chino/api.py
+++ chino/api.py
@@ -24,7 +24,7 @@ from os import path
 __author__ = 'Stefano Tranquillini <stefano@chino.io>'
 
 logger = logging.getLogger('chino')
-logging.config.fileConfig('logging.conf')
+# logging.config.fileConfig(path.join([path.dirname(__file__),'logging.conf']))
 
 class ChinoAPIBase(object):  # PRAGMA: NO COVER
     '''
