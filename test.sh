5e2d2ff3d57f72883bee7bfdeede7b6e53ec820d
Stefano Tranquillini <stefano.tranquillini@gmail.com>
2017-01-25 14:30:23 +0100

    some tesitng

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd test
 nosetests . --with-coverage --cover-package=chino --logging-level=ERROR --stop
+say "test over"
+osascript -e 'display notification "Done, good or bad?" with title "Test over"'
