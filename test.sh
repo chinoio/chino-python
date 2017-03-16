e8c958230faf221ba87e75940b59a5687baebe0c
Stefano Tranquillini <stefano.tranquillini@gmail.com>
2017-03-16 10:48:06 +0100

    adding trevis support

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+source test.env
 cd test
-nosetests . --with-coverage --cover-package=chino --stop
+nosetests tests_chino.py --with-coverage --cover-html --cover-package=chino --stop
 say "test over"
 osascript -e 'display notification "Done, good or bad?" with title "Test over"'
+#nosetests -s tests_chino_data.py
