61c5a1a9f9e970bbfd56747c6e7a742b22469bc7
Andrea <andrea@chino.io>
2018-02-21 12:26:24 +0100

    Created test data

---
+++ test/consent_data.py
@@ -0,0 +1,59 @@
+consent_list = [
+# First test class
+  {
+    "user_id": "user1@mail.com",
+    "description": "This policy was created for test purposes. The policy url is linked to Chino.io policy.",
+    "policy_url": "https://www.chino.io/legal/privacy-policy",
+    "policy_version": "v1",
+    "collection_mode": "none",
+    "data_controller": {
+      "company" : "Chino.io",
+      "contact" : "controller",
+      "address" : "Via S.G. Bosco 27, 38068 Rovereto",
+      "email" : "controller@mail.com",
+      "VAT" : "n/d",
+      "on_behalf" : true
+    },
+    "purposes": [
+      {
+          "authorized": true,
+          "purpose": "testing",
+          "description" : "Testing class api.ChinoAPIConsents"
+      },
+      {
+          "authorized": false,
+          "purpose": "testing",
+          "description" : "Testing class objects.Consent"
+      }
+    ]
+  },
+
+# Second test class
+  {
+    "user_id": "user2@mail.com",
+    "description": "This policy was created for test purposes. The policy url is linked to Chino.io policy.",
+    "policy_url": "https://www.chino.io/legal/privacy-policy",
+    "policy_version": "v1",
+    "collection_mode": "none",
+    "data_controller": {
+      "company" : "Chino.io",
+      "contact" : "controller",
+      "address" : "Via S.G. Bosco 27, 38068 Rovereto",
+      "email" : "controller@mail.com",
+      "VAT" : "n/d",
+      "on_behalf" : true
+    },
+    "purposes": [
+      {
+          "authorized": true,
+          "purpose": "testing",
+          "description" : "Testing class api.ChinoAPIConsents"
+      },
+      {
+          "authorized": false,
+          "purpose": "testing",
+          "description" : "Testing class objects.Consent"
+      }
+    ]
+  }
+]
